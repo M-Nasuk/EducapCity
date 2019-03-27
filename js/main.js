b6e3e790c32d05742e417ba09d08a458c26bff8c
M-Nasuk <m.kusanagi@gmx.fr>
2019-03-27 18:38:18 +0100

    Added Flexslider and main picture.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ document.addEventListener('DOMContentLoaded', ()=> {
   let body = document.querySelector('body');
 
   $.get('resources/src/header.php')
-  .done(function(response) {$('body').prepend(response)});
+  .done(function(response) {
+    $('body').prepend(response);
+    $('#menu > ul > li a h3').addClass('jah_font');
+    $('#menu > ul > li ul li').addClass('jah_font');
+  });
 
   $(document).on('click', '#b_menu', function() {
     if($('nav').css('display') == 'none') {
@@ -11,9 +15,58 @@ document.addEventListener('DOMContentLoaded', ()=> {
     } else {
       $('nav').css('display', 'none');
     };
-  })
+  });
 
-  // $(document).on('click', '#menu > ul li a', function() {
-  //   $(this).nextUntil('li').toggle();
-  // })
+  $.get('resources/src/main.php')
+  .done(function(response) {
+    $('body').append(response);
+    $('#accueil_area').addClass('jah_font');
+    $('.flexslider').flexslider({
+      animation: "slide",
+      video: true
+    });
+  });
+
+  // var player, firstScriptTag, tag = document.createElement('script');
+  //
+  // //async youtube api script reference
+  // tag.src = "https://www.youtube.com/iframe_api";
+  // firstScriptTag = document.getElementsByTagName('script')[0];
+  // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  //
+  // function onYouTubeIframeAPIReady() {
+  //    player = new YT.Player('player', {
+  //        width: 960,
+  //        height: 540,
+  //        videoId: 'XXXX',
+  //        playerVars: { 'autoplay': 0, 'showinfo': 0, 'loop': 1, 'rel': 0, 'vq': 'hd1080', 'controls': 0 },
+  //        events: {
+  //            'onStateChange': function(event) {
+  //                 controlSlider(event);
+  //            }
+  //        }
+  //     });
+  // };
+  //
+  // function toggleVideo(state) {
+  //   if(state == "pause") {
+  //       player.pauseVideo();
+  //   } else {
+  //       player.playVideo();
+  //   }
+  // }
+  //
+  // function controlSlider(event) {
+  //
+  //   var playerstate=event.data;
+  //   console.log(playerstate);
+  //   if(playerstate==1 || playerstate==3){
+  //       $('.flexslider.main').flexslider("pause");
+  //   }
+  //   if(playerstate==2){
+  //       $('.flexslider.main').flexslider("play");
+  //   }
+  // }
+
+
 })
---
+++ resources/src/main.php
@@ -0,0 +1,25 @@
+<main>
+  <section id="accueil_area">
+    <h2>EDUCAP CITY, C'EST QUOI ?</h2>
+    <p>Un tour de France de la citoyenneté, du civisme et de la fraternité pour les jeunes de 9 à 14ans</p>
+  </section>
+  <section id="slider">
+    <div class="flexslider">
+      <ul class="slides">
+        <li>
+          <iframe id="js-youtube_1" src="https://www.youtube.com/watch?v=8bDsB-XZRrE?enablejsapi=1&amp;controls=1&amp;showinfo=0" width="500" height="281" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+        </li>
+        <li>
+          <img src="resources/images/sliders/slide1_tournee.png" alt="slide1_tournee" />
+        </li>
+        <li>
+          <img src="resources/images/sliders/slide2_videosRallye.png" alt="slide2_videosRallye" />
+        </li>
+        <li>
+          <img src="resources/images/sliders/slides1_inscriptionCapitale.jpg" alt="slides1_inscriptionCapitale" />
+        </li>
+      </ul>
+    </div>
+  </section>
+
+</main>

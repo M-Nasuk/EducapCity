88a3768b5d8192b9558e1356e2fae4f9362aae1e
M-Nasuk <m.kusanagi@gmx.fr>
2019-04-04 18:02:03 +0200

    SMTP mail client done.

--- test.php
+++ test.php
@@ -1,59 +1,54 @@
 <?php
 
-// $json = json_decode('
-// {"name":"Jo","email":"sophia@capsaa.net","subject":"Test 12 milles","content":"Hello :)"}', true);
-//
-// $text = wordwrap($json['content'], 70, "\r\n");
-// $message = str_replace("\n.", "\n..", $text);
-//
-// if (mail('niazz@hotmail.fr', 'test', 'test contenu', 'test@live.fr')) {
-//   echo 'success';
-// } else {
-//   echo 'error';
-// }
-// var_dump($json);
-// echo $json['name'];
-//
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
 
 require 'vendor/autoload.php';
 
 $mail = new PHPMailer(true);
 
 try {
-    //Server settings
-    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
-    $mail->isSMTP();                                            // Set mailer to use SMTP
-    $mail->Host       = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
-    $mail->SMTPAuth   = false;                                   // Enable SMTP authentication
-    $mail->Username   = 'user@example.com';                     // SMTP username
-    $mail->Password   = 'secret';                               // SMTP password
-    $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
-    $mail->Port       = 587;                                    // TCP port to connect to
+  /* Set the mail sender. */
+  $mail->setFrom('de', 'Me');
 
-    //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('niazz@hotmail.fr', 'Niazz');     // Add a recipient
-    // $mail->addAddress('ellen@example.com');               // Name is optional
-    // $mail->addReplyTo('info@example.com', 'Information');
-    // $mail->addCC('cc@example.com');
-    // $mail->addBCC('bcc@example.com');
+  /* Add a recipient. */
+  $mail->addAddress('a', 'Me');
 
-    // Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+  $mail->addReplyTo('repond a', 'Me');
 
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+  /* Set the subject. */
+  $mail->Subject = 'Force';
 
-    $mail->send();
-    echo 'Message has been sent';
+  /* Tells PHPMailer to use SMTP. */
+  $mail->isSMTP();
+
+  /* SMTP server address. */
+  $mail->Host = 'any smtp config';
+
+  /* Use SMTP authentication. */
+  $mail->SMTPAuth = TRUE;
+
+  /* Set the encryption system. */
+  $mail->SMTPSecure = 'tls';
+
+  /* SMTP authentication username. */
+  $mail->Username = 'mail@mail.com';
+
+  /* SMTP authentication password. */
+  $mail->Password = 'password';
+
+  /* Set the SMTP port. */
+  $mail->Port = 587;
+
+  /* Set the mail message body. */
+  $mail->isHTML(TRUE);
+  $mail->Body = '<html>There is a great disturbance in the <strong>Force</strong>.</html>';
+  $mail->AltBody = 'There is a great disturbance in the Force.';
+
+  /* Finally send the mail. */
+  $mail->send();
+
+  echo 'Message has been sent';
 } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+  echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }

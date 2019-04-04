4f6590b1416296112e1f2fc83b7752b2c475641f
M-Nasuk <m.kusanagi@gmx.fr>
2019-04-04 15:35:18 +0200

    back to mail()

--- resources/src/send_form.php
+++ resources/src/send_form.php
@@ -1,23 +1,20 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
 $fetch = json_decode(file_get_contents('php://input'), true);
 
 $text = wordwrap($fetch['content'], 70, "\r\n");
 $message = str_replace("\n.", "\n..", $text);
 
-// $sender_name = $fetch['name'];
-// $sender_mail = $fetch['email'];
-// $headers = [
-//   "From" => "$sender_name <$sender_mail>",
-//   "Reply-To" => "pc@example.com",
-//   "Content-type" => "text/plain; charset=iso-8859-1",
-//   "MIME-Version" => "1.0"
-// ];
-//
-// 
-// $send = mail('niazz@hotmail.fr', $fetch['subject'], $message, $headers);
+$sender_name = $fetch['name'];
+$sender_mail = $fetch['email'];
+$headers = [
+  "From" => "$sender_name <$sender_mail>",
+  "Reply-To" => "pc@example.com",
+  "Content-type" => "text/plain; charset=iso-8859-1",
+  "MIME-Version" => "1.0"
+];
+
+
+$send = mail('jo@example.net', $fetch['subject'], $message, $headers);
 
 echo json_encode($send);

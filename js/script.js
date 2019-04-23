beb20040c2305e55588f1a4c634e79772daf9cd5
M-Nasuk <m.kusanagi@gmx.fr>
2019-04-23 15:43:57 +0200

    Added commit to js functions.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
+// Function that pauses the flexslider animation
 function pauseslider() {
   $('#slider').flexslider("pause");
 }
+
+// Function that play the flexslider animation
 function playslider() {
   $('#slider').flexslider("play");
 }
+
+// Function that resume the flexslider animation
 function resumeslider() {
   $('#slider').flexslider("next"); $('#slider').flexslider("play");
 }
 
+// Funtion that launch a counter on a specific DOM Element, when scrolled over on the screen.
 function startCounter() {
   if ($(window).scrollTop() > 300) {
     $(window).off("scroll", startCounter);
@@ -25,11 +31,13 @@ function startCounter() {
   }
 }
 
+// Function that scroll to a specific anchor on the page.
 function scrollToAnchor(sid){
   let sTag = $("#"+ sid + "");
   $('html,body').animate({scrollTop: sTag.offset().top},'slow');
 }
 
+// Function that put the aside block at a certain position on every page except home page, on desktop view
 function alterAside() {
   $('aside:not(#body_accueil aside)').css({
     "top": "300px"

343fb441a106be12bc9e7fa1a44dbe3256a16571
M-Nasuk <m.kusanagi@gmx.fr>
2019-04-30 15:46:32 +0200

    Add doc about adding any city on the map.

--- resources/src/page_nos_editions.php
+++ resources/src/page_nos_editions.php
@@ -1,3 +1,6 @@
 <main id="nos_editions">
+
+  <!-- Pour ajouter une ville(étape) sur la map, allez sur https://www.google.com/maps/d/?hl=fr avec le compte d'educapcity, choisir Carte Tournee 2019,
+  ensuite ajoutez la ville de votre choix sur le calque de votre choix (Cap Classe, Cap Rallye, etc...). -->
   <iframe src="https://www.google.com/maps/d/embed?mid=13097Ul8cRmvzdmu0o7ozDoHMFZ8Viyip&hl=fr"></iframe>
 </main>

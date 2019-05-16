6a2a612012e00ae666a3318b75cca688ae295a29
M-Nasuk <m.kusanagi@gmx.fr>
2019-05-16 13:58:46 +0200

    Logo partenaire flexé !

--- resources/src/participer/page_partenaire.php
+++ resources/src/participer/page_partenaire.php
@@ -4,15 +4,18 @@
   </div>
   <div class="partenaire">
     <p class="strong">Partenaires financiers et logistiques</p>
-    <p>En plus de soutenir par des dons financiers ou en nature le programme EDUCAP CITY, nous sommes heureux de pouvoir compter sur les compétences, l’implication et le réseau de chacun de nos partenaires.</p>
-    <p>Ils nous accompagnent dans la mise en œuvre du programme et contribuent aux orientations à travers leur expertise et les liens que nous tissons sur le terrain.</p>
-    <p>Grâce à eux, les dispositifs CAP Classe et CAP Rallye ont déjà bénéficié à plus de 40 000 jeunes et continuent de se développer sur tout le territoire.</p>
-  </div>
-  <div class="">
-    <p>Soutenir EDUCAP CITY, c’est aussi proposer des opportunités d’engagement pour les collaborateurs, bénéficier de retours positifs pour sa marque et valoriser ses actions auprès des plus jeunes.</p>
-    <h2 class="jah_font pre">Vous souhaitez devenir
-      partenaire ?</h2>
-    <button class="bg-yellow round-border">Contactez-nous</button>
+    <div class="">
+
+      <p>En plus de soutenir par des dons financiers ou en nature le programme EDUCAP CITY, nous sommes heureux de pouvoir compter sur les compétences, l’implication et le réseau de chacun de nos partenaires.</p>
+      <p>Ils nous accompagnent dans la mise en œuvre du programme et contribuent aux orientations à travers leur expertise et les liens que nous tissons sur le terrain.</p>
+      <p>Grâce à eux, les dispositifs CAP Classe et CAP Rallye ont déjà bénéficié à plus de 40 000 jeunes et continuent de se développer sur tout le territoire.</p>
+    </div>
+    <div class="">
+      <p>Soutenir EDUCAP CITY, c’est aussi proposer des opportunités d’engagement pour les collaborateurs, bénéficier de retours positifs pour sa marque et valoriser ses actions auprès des plus jeunes.</p>
+      <h2 class="jah_font pre">Vous souhaitez devenir
+        partenaire ?</h2>
+      <button class="bg-yellow round-border">Contactez-nous</button>
+    </div>
   </div>
   <div id="div_stmichel">
     <div>
